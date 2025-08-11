1f9ae10314a3422740abedeb64313b1588eb306d
Nanthu Praseed Sasikumar <nanthups.logezy@gmail.com>
2025-08-12 03:02:46 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -2,40 +2,28 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue({ customElement: true })],
-  define: {
-    "process.env.NODE_ENV": JSON.stringify("production"),
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        "process.env.NODE_ENV": '"production"',
-      },
-    },
-  },
+  plugins: [
+    vue({
+      customElement: true,
+    }),
+  ],
   build: {
     target: "es2018",
     minify: "esbuild",
-    cssCodeSplit: false,
     lib: {
       entry: "src/main.js",
       name: "LogezyWidgets",
       formats: ["iife", "esm"],
-      fileName: (format) =>
-        format === "iife" ? "widgets.min.js" : `widgets.${format}.js`,
+      fileName: (f) => (f === "iife" ? "widgets.min.js" : `widgets.${f}.js`),
     },
     rollupOptions: {
       inlineDynamicImports: true,
       output: {
         manualChunks: undefined,
-        banner: `;(function() {
-          if (typeof window !== 'undefined') {
-            if (!window.process) {
-              window.process = { env: { NODE_ENV: 'production' } };
-            } else if (!window.process.env) {
-              window.process.env = { NODE_ENV: 'production' };
-            }
-          }
+        banner: `;(function(){var w=typeof window!=='undefined'?window:self;
+          if(typeof w.process==='undefined')w.process={env:{NODE_ENV:'production'}};
+          else if(typeof w.process.env==='undefined')w.process.env={NODE_ENV:'production'};
+          if(typeof w.global==='undefined')w.global=w;
         })();`,
       },
     },
