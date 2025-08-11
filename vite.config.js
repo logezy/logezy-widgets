392d1016cda85d206e756a1014062ddb08cf5e2b
Nanthu Praseed Sasikumar <nanthups.logezy@gmail.com>
2025-08-11 20:01:39 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     }),
   ],
   define: {
-    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
+    'process.env.NODE_ENV': '"production"',
   },
   build: {
     target: "es2018",
